a3a99ec9c1c914a7ed1915e77dc1daa7a57e1f43
Dirk Jansen van Rensburg <dirk.jvrensburg@arwyp.com>
2021-04-09 15:17:35 +0200

    added port to server

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
-BEGIN TRANSACTION;
+-- BEGIN TRANSACTION;
 
-INSERT into users (name,email,entries,joined) values('dirk','dirk@gmail.com',5,'2021-03-11');
-INSERT into login (hash,email) values('$2a$04$9BxGS4WkWJeU5fHDUySwr.0lZ3tHi1lN8sPbhSevzO/XN6eeBtX16', 'dirk@gmail.com' );
+-- -- INSERT into users (name,email,entries,joined) values('dirk','dirk@gmail.com',5,'2021-03-11');
+-- -- INSERT into login (hash,email) values('$2a$04$9BxGS4WkWJeU5fHDUySwr.0lZ3tHi1lN8sPbhSevzO/XN6eeBtX16', 'dirk@gmail.com' );
 
-COMMIT;
+-- COMMIT;
